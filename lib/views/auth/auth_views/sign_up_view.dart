50e066a0358b3170df64351f644135c9caf0219d
dev one <dev@test.com>
2024-03-20 23:22:16 +0300

    most auth section done

---
+++ lib/views/auth/auth_views/sign_up_view.dart
@@ -0,0 +1,184 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:public_chat_app/constants/spacing.dart';
+import 'package:public_chat_app/extension/app_extension.dart';
+import 'package:public_chat_app/views/auth/auth_views/log_in_view.dart';
+import 'package:public_chat_app/views/auth/bloc/auth_bloc.dart';
+
+// ignore: must_be_immutable
+class SignUpView extends StatelessWidget {
+  SignUpView({super.key});
+  TextEditingController nameController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    final bloc = context.read<AuthBloc>();
+    return BlocConsumer<AuthBloc, AuthState>(
+      listener: (context, state) {
+        if (state is ErrorState) {
+          context.showErrorSnackBar(
+            state.msg,
+          );
+        }
+        if (state is SuccessState) {
+          context.pushAndRemove(LoginView());
+        }
+      },
+      builder: (context, state) {
+        return Scaffold(
+          resizeToAvoidBottomInset: false,
+          backgroundColor: Colors.white,
+          body: SafeArea(
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      CircleAvatar(
+                        radius: 50,
+                        backgroundColor: Color.fromARGB(255, 255, 156, 110),
+                        child: Icon(
+                          Icons.person_4_outlined,
+                          size: 70,
+                          color: Colors.white,
+                        ),
+                      ),
+                      kH16,
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Great to have you!",
+                            style: TextStyle(
+                              fontSize: 25,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            "Create an account",
+                            style: TextStyle(
+                              fontSize: 20,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  kV16,
+                  Container(
+                    width: context.getWidth(),
+                    height: context.getHigh() * 0.5,
+                    margin: const EdgeInsets.symmetric(horizontal: 20),
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 20,
+                      horizontal: 15,
+                    ),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Column(
+                      children: [
+                        TextField(
+                          controller: nameController,
+                          decoration: const InputDecoration(
+                            labelText: "Name",
+                            border: OutlineInputBorder(),
+                          ),
+                        ),
+                        kV8,
+                        TextField(
+                          controller: emailController,
+                          decoration: const InputDecoration(
+                            labelText: "Email",
+                            border: OutlineInputBorder(),
+                          ),
+                        ),
+                        kV8,
+                        TextField(
+                          controller: passwordController,
+                          decoration: const InputDecoration(
+                            labelText: "Password",
+                            border: OutlineInputBorder(),
+                          ),
+                        ),
+                        kV16,
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            InkWell(
+                              onTap: () {
+                                try {
+                                  bloc.add(
+                                    SignUpEvent(
+                                      name: nameController.text,
+                                      email: emailController.text,
+                                      password: passwordController.text,
+                                    ),
+                                  );
+                                  context.pushAndRemove(LoginView());
+                                } catch (_) {
+                                  print("error");
+                                }
+                              },
+                              child: Container(
+                                padding: const EdgeInsets.symmetric(
+                                  horizontal: 15,
+                                  vertical: 5,
+                                ),
+                                decoration: BoxDecoration(
+                                  color:
+                                      const Color.fromARGB(255, 76, 180, 156),
+                                  borderRadius: BorderRadius.circular(20),
+                                ),
+                                child: const Text(
+                                  "Sign in",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            kH8,
+                            InkWell(
+                              onTap: () {
+                                context.popNavigator();
+                              },
+                              child: Container(
+                                padding: const EdgeInsets.symmetric(
+                                  horizontal: 15,
+                                  vertical: 5,
+                                ),
+                                decoration: BoxDecoration(
+                                  color: Colors.yellow.shade200,
+                                  borderRadius: BorderRadius.circular(20),
+                                ),
+                                child: const Text(
+                                  "Log in",
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
--- lib/views/redirector_view.dart
+++ lib/views/redirector_view.dart
@@ -1,26 +1,30 @@
-// import 'package:flutter/material.dart';
-// import 'package:public_chat_app/views/auth/bloc/auth_bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:public_chat_app/extension/app_extension.dart';
+import 'package:public_chat_app/views/auth/auth_views/log_in_view.dart';
+import 'package:public_chat_app/views/auth/bloc/auth_bloc.dart';
+import 'package:public_chat_app/views/chat/chat_view.dart';
 
-// class RedirectView extends StatelessWidget {
-//   const RedirectView({super.key});
+class RedirectView extends StatelessWidget {
+  const RedirectView({super.key});
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return BlocListener<AuthBloc, AuthState>(
-//       listener: (context, state) {
-//         if (state is AvailableSessionState) {
-//           if (state.isSessionAvailable != null) {
-//             // context.pushAndRemove( const ChatView());
-//           } else {
-//             // context.pushAndRemove(LoginView());
-//           }
-//         }
-//       },
-//       child: const Scaffold(
-//         body: Center(
-//           child: Text("This is a Redirect view"),
-//         ),
-//       ),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return BlocListener<AuthBloc, AuthState>(
+      listener: (context, state) {
+        if (state is AvailableSessionState) {
+          if (state.isSessionAvailable != null) {
+            context.pushAndRemove( const ChatView());
+          } else {
+            context.pushAndRemove(LoginView());
+          }
+        }
+      },
+      child: const Scaffold(
+        body: Center(
+          child: Text("This is a Redirect view"),
+        ),
+      ),
+    );
+  }
+}
